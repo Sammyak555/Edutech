10599d51e594611863c544b133dac85538ace790
Sammyak555 <sammyak.deosale.1@gmail.com>
2023-01-27 19:46:22 +0530

    reg/log

--- Backend/Config/db.js
+++ Backend/Config/db.js
@@ -1,7 +1,7 @@
 const mongoose = require('mongoose')
 // require('dotenv').config()
 
-const connection = mongoose.connect('mongodb://127.0.0.1:27017/')
+const connection = mongoose.connect('mongodb://127.0.0.1:27017/edutech')
 
 module.exports={
     connection
--- Backend/index.js
+++ Backend/index.js
@@ -1,11 +1,46 @@
 const express = require('express')
 const cors = require('cors')
 const { connection } = require('./Config/db')
+const { userRouter } = require('./Routes/user.routes')
 // require('dotenv').config()
 const app=express()
 app.use(cors())
+// const nodeMailer = require('nodemailer')
+
+// const html =`
+//     <h1>Hi</h1>
+//     <p>node mailer</p>
+// `;
+// const emails=[
+//     'nikhilwalwatkar81@gmail.com','sammyak.deosale.1@gmail.com'
+// ]
+// async function main(){
+//    const transporter = nodeMailer.createTransport({
+//         host:'sammyakdeosaleverify@gmail.com',
+//         port:465,
+//         secure:true,
+//         auth:{
+//             user:'sammyakdeosaleverify@gmail.com',
+//             pass:'sammyak123'
+//         }
+//     })
+//    const info = await transporter.sendMail({
+//         from:'sammyak <sammyakdeosaleverify@gmail.com>',
+//         to:emails,
+//         subject:'testing mail verification',
+//         html:html
+//     })
+
+//     console.log("msg sent:"+info.messageId);
+//     console.log(info.accepted)
+//     console.log(info.rejected)
+
+// }
+// main()
+// .catch((e)=>console.log(e))
 
 app.use(express.json())
+app.use('/users',userRouter)
 
 app.listen(5000,async(req,res)=>{
     try{
