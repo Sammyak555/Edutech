b8c3a05b4c9ab3d3dd9ba877cf4eaf7b4afe0b39
Sammyak555 <sammyak.deosale.1@gmail.com>
2023-01-28 16:53:54 +0530

    l&g

--- edutech/src/pages/home.tsx
+++ edutech/src/pages/home.tsx
@@ -8,6 +8,23 @@ const home = () => {
             <h1>Welcome, sammyak! 👋🏼</h1>
             <p>Get access to quality study materials, brush up on concepts, solve sample papers and clarify all your doubts.</p>
         </div>
+        <div className='why-info'>
+            <h1>Why Learn & Grow ?</h1>
+            <div className='why-sub'>
+            <div style={{textAlign:"center",justifyContent:'center',alignItems:"center"}}>
+                <img src="https://www.edutech.com/tools/img/why/lifelong.png" alt="" />
+                <h1 style={{width:"120px"}}>Lifelong Learning</h1>
+            </div>
+            <div style={{textAlign:"center",justifyContent:'center',alignItems:"center"}}>
+                <img src="https://www.edutech.com/tools/img/why/deep.png" alt="" />
+                <h1 style={{width:"120px"}}>Deep Understanding</h1>
+            </div>
+            <div style={{textAlign:"center",justifyContent:'center',alignItems:"center"}}>
+                <img src="https://www.edutech.com/tools/img/why/future.png" alt="" />
+                <h1>Future Forward</h1>
+            </div>
+            </div>
+        </div>
       <div className='panelnav'>
         <div className='subpanel'>
         <Link className='head-titles' href={'/maths'}><img  src='https://cdn-icons-png.flaticon.com/128/897/897406.png' alt=''></img>Mathematics</Link>
