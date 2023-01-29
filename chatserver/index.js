b905292ebc70194dd7980b0d4bbd38fec3e44fdd
SauravKumar <sk729584@gmail.com>
2023-01-29 12:23:44 +0530

    add chat app in science

--- chatserver/index.js
+++ chatserver/index.js
@@ -1,51 +1,51 @@
-const http=require("http");
-const express =require("express");
+const http = require("http");
+const express = require("express");
 const cors = require("cors");
 const socketIO = require("socket.io");
-const { connection } = require("../Backend/Config/db");
 
-const app=express();  // creating the server 
-const port=4500;
+const app = express(); // creating the server
+const port = 4500;
 
-
-const users=[{}];
+const users = [{}];
 
 app.use(cors());
-app.get("/",(req,res)=>{
-    res.send("HELL ITS WORKING");
-    
-})
-
-const server=http.createServer(app);
-
-const io=socketIO(server);  //   making a connection io
-
-io.on("connection",(socket)=>{
-    console.log("New Connection");
-
-    socket.on('joined',({user})=>{
-          users[socket.id]=user;
-          console.log(`${user} has joined `);
-          socket.broadcast.emit('userJoined',{user:"Admin",message:` ${users[socket.id]} has joined`});
-          socket.emit('welcome',{user:"Admin",message:`Welcome to the chat,${users[socket.id]} `})
-    })
-
-    socket.on('message',({message,id})=>{
-        io.emit('sendMessage',{user:users[id],message,id});
-    })
-
-    socket.on('disconnect',()=>{
-          socket.broadcast.emit('leave',{user:"Admin",message:`${users[socket.id]}  has left`});
-        console.log(`user left`);
-    })
+app.get("/", (req, res) => {
+  res.send("HELL ITS WORKING");
 });
 
+const server = http.createServer(app);
 
-server.listen(port,async()=>{      // listing to server 
-    try{
-        await connection
-    }catch(err){
-        console.log(err)
-    }
-    console.log(`Working`);
-})
+const io = socketIO(server); //   making a connection io
+
+io.on("connection", (socket) => {
+  console.log("New Connection");
+
+  socket.on("joined", ({ user }) => {
+    users[socket.id] = user;
+    console.log(`${user} has joined `);
+    socket.broadcast.emit("userJoined", {
+      user: "Admin",
+      message: ` ${users[socket.id]} has joined`,
+    });
+    socket.emit("welcome", {
+      user: "Admin",
+      message: `Welcome to the chat,${users[socket.id]} `,
+    });
+  });
+
+  socket.on("message", ({ message, id }) => {
+    io.emit("sendMessage", { user: users[id], message, id });
+  });
+
+  socket.on("disconnect", () => {
+    socket.broadcast.emit("leave", {
+      user: "Admin",
+      message: `${users[socket.id]}  has left`,
+    });
+    console.log(`user left`);
+  });
+});
+
+server.listen(port, () => {
+  console.log(`Working`);
+});
